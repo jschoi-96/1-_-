1908c638d12ca2370735743c813c3d6c8ff5cc15
junsoo <choix712@umn.edu>
2023-10-17 17:03:22 +0900

    feat: 숫자를 입력하세요: 라는 문구를 출력한다

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class OutputView {
+
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요: ";
+
+    public void getInputNumber() {
+        System.out.println(INPUT_NUMBER);
+    }
+}
