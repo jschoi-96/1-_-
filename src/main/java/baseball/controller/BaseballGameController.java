e4bfe3389946a89c5a89f3ef5f2c5ab340270c44
junsoo <choix712@umn.edu>
2023-10-18 18:35:32 +0900

    refactor: input값을 담는 Player 클래스 생성

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,15 +1,20 @@
 package baseball.controller;
 
-import baseball.view.InputView;
-import baseball.view.OutputView;
+import baseball.model.ValidateInput;
+import baseball.view.View;
 
+import camp.nextstep.edu.missionutils.Console;
 public class BaseballGameController {
 
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-
+    View outputView = new View();
     public void startGame() {
         outputView.getInputNumber();
-        inputView.getInputNumber();
+        getInputNumber();
+    }
+
+    public String getInputNumber() {
+        String input = Console.readLine();
+        ValidateInput.validateController(input);
+        return input;
     }
 }
