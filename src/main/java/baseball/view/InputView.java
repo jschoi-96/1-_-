8accf30bddeff6cb65189c43b1b0a552df6da523
junsoo <choix712@umn.edu>
2023-10-18 14:11:29 +0900

    feat: 중복되는 숫자가 있을 때 예외 사항

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 import baseball.model.Computer;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -20,6 +21,16 @@ public class InputView {
     private static void validateSize(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("플레이어는 세자리 수를 입력해야 합니다.");
+        }
+    }
+
+    private static void validateDuplicate(String input) {
+        Set<Character> duplicate = new HashSet<>();
+        for(int i = 0; i < input.length(); i++){
+            duplicate.add(input.charAt(i));
+        }
+        if (duplicate.size() != 3) {
+            throw new IllegalArgumentException("중복되는 숫자를 입력값으로 받을 수 없습니다.");
         }
     }
 
